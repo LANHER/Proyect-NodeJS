c0c01273f91e2efe1f8b4c9dfe2d2cdc662b3e58
DavidDonnet <dlanderos10@alumnos.uaq.mx>
2020-06-07 01:08:09 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//Dependencies
+const morgan = require('morgan');
+const express= require('express');
+const app = express();
+//Routers
+const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
+//Middleware
+const auth = require('./middleware/auth');
+const notFoud = require('./middleware/notFound')
+const index = require('./middleware/index');
+const cors = require('./middleware/cors')
+
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+ 
+app.get("/",index);
+app.use("/user", user);
+app.use(auth);
+app.use("/pokemon",pokemon);
+app.use(notFoud);
+
+app.listen(process.env.PORT||3000,()=>{
+    console.log("Server is Running...");
+});
